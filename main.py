fc7dbe46c0655dae06f3fee388d34278fcb44645
sAm <m.a.mofidian@hotmail.co.uk>
2023-01-27 17:04:42 +0330

    add welcome msg and comments for run func

--- main.py
+++ main.py
@@ -11,13 +11,21 @@ def run():
     select playing algorithm
     :return:
     """
+    # initial message
+    print("*"*51)
+    print(" "*20 + "Tic-Tac-Toe" + " "*20)
+    print("*"*51)
+    # init game
     game = Game()
+    # choose algorithm(minimax vs alpha-beta)
     alg = input("Choose the algorithm\n\t1: Alpha-Beta\n\t2: minimax\nEnter the algorithm number: ")
     if alg == '1':
         game.play_alpha_beta()
     elif alg == '2':
         game.play_minimax()
+    # invalid input
     else:
+        print("Invalid input!")
         return run()
 
 
